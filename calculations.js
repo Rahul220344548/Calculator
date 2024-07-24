d7c294fa441d9ed0df6cfbb6bb0fffd673aa56f4
Rahul Vasantlal <220344548@aston.ac.uk>
2024-07-24 16:15:09 +0100

    working on the logic for updating display for second input

--- calculations.js
+++ calculations.js
@@ -1,4 +1,7 @@
-
+let firstNum = '';
+let secondNum = '';
+let operator;
+let enterSecondNumFlag = false;
 
 function add(inFirstNum,inSecondNum) {
     return inFirstNum + inSecondNum;
@@ -17,36 +20,52 @@ function divide(inFirstNum,inSecondNum) {
 }
 
 function operate(operator, firstNum, secondNum) {
-    console.log(operator);
-    if (operator == "+") {
-        console.log(add(firstNum,secondNum));
-    }
+    console.log(firstNum+ " " + operator + " " + secondNum);
+    // if (operator == "+") {
+    //     console.log(add(firstNum,secondNum));
+    // }
     
 }
 
-function displayDigits(value) {
+function setDisplay(value) {
 
-    const displayDigit = document.querySelector(".display .display-digits");
-    if (displayDigit) {
-        displayDigit.textContent += value;  
-    }
-    firstNum = displayDigit.textContent;
     
+    const currInput = document.querySelector(".display .display-digits");
+      
+    if (enterSecondNumFlag) {
+        currInput.textContent = value;
+        enterSecondNumFlag = false;
+    } else {
+        if (currInput.textContent === '0') {
+            currInput.textContent = value;
+        } else {
+            currInput.textContent += value;
+        }
+    }
+    
+    //If no operator has been selected yet, update the first number
+    if (!operator) {
+        firstNum = currInput.textContent;
+    } else {
+        secondNum = currInput.textContent;
+    }
+
 }
 
-// NON FUNCTION CODE ??????????????????????????????????
-
-let firstNum;
-let secondNum;
-let operator="+";
-
+function storeOperator(inOP) {
+    if (firstNum) {
+        operator = inOP;
+        enteringSecondNum = true;  // Prepare for the second number input
+    }
+    console.log("Operator stored: ", operator);
+}
 
 const digitButtons = document.querySelectorAll('.button-digit');
 
 digitButtons.forEach(button => {
     button.addEventListener('click', () => {
         const displayValue = button.textContent;
-        displayDigits(displayValue);
+        setDisplay(displayValue);
     });
 })
 
@@ -57,6 +76,19 @@ operatorSelect.forEach(button => {
 
     button.addEventListener('click', () => {
         const operatorValue = button.textContent;
-        operate(operatorValue);
+        console.log(storeOperator(operatorValue));
     });
+});
+
+
+const equal = document.querySelector('.button-equal');
+equal.addEventListener('click', () => {
+    operate(operator,firstNum,secondNum);
+});
+
+
+const clearDisplay = document.querySelector('.button-clr');
+clearDisplay.addEventListener('click', () => {
+    const currInput = document.querySelector(".display .display-digits");
+    setTimeout(() => window.location.reload(), 100);
 });
